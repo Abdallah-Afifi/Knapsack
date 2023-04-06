2fc55f6c0a26aef2318dd5ea7889bed1af10f276
AbdallahFathi22 <114703186+AbdallahFathi22@users.noreply.github.com>
2023-04-06 10:46:13 +0200

    Add files via upload

---
+++ function1.h
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+
+// A utility function that returns maximum of two integers
+int max(int a, int b);
+int knapSack(int W, int wt[], int val[], int n);
+
